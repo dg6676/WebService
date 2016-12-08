5c3dd2389c5d13d0cc104c39749c11352628fc7c
Kim Min Ju <77aswn@naver.com>
2016-12-08 17:21:31 +0900

    a

--- routes/member.js
+++ routes/member.js
@@ -9,15 +9,23 @@ var router = express.Router();
 router.get('/:menu?', function(req, res, next) {
     var menu = req.params.menu;
     if(menu == 'login'){
-        res.render('login', { title: 'Express' });
+        res.render('login');
     }else if(menu == 'join'){
-        res.render('signup', { title: 'Express' });
+        res.render('signup');
     }else
         next();
 });
 
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express' });
+});
+
+router.post('/signup', function(req, res, next){
+
+});
+
+router.post('/login', function(req, res, next){
+
 });
 
 module.exports = router;
