2ff30725b0c39a2dc4426e34bcd4afdb34b9711d
Kim Min Ju <77aswn@naver.com>
2016-12-12 02:25:00 +0900

    routes

--- routes/myPage.js
+++ routes/myPage.js
@@ -3,10 +3,28 @@
  */
 var express = require('express');
 var router = express.Router();
+var db = require('../public/scripts/dbAccess');
+var session = require('express-session');
 
-/* GET home page. */
+router.get('/:menu?', function(req, res, next){
+    var menu = req.params.menu;
+    if(menu == 'incorrect'){
+        db.getUserIncorrectQuestion(req.session.userInfo.userID, function(list){
+            res.render('', {'title': 'user incorrect list', qList: list});
+        });
+    }else if(menu == 'myquestion'){
+        db.getUserQuestion(req.session.userInfo.userID, function(list){
+            res.render('', {'title': 'user question', qList: list})
+        });
+    }else next();
+});
+
 router.get('/', function(req, res, next) {
-    res.render('');
+    res.render('myPage');
+});
+
+router.post('/', function(req, res, next){
+   //회원 정보 수정
 });
 
 module.exports = router;
