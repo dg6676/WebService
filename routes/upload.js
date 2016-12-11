b6b6c7f78d79acb48b37d5d894b5038e01943a5f
Kim Min Ju <77aswn@naver.com>
2016-12-11 23:52:23 +0900

    routes

--- routes/upload.js
+++ routes/upload.js
@@ -20,13 +20,13 @@ router.get('/', function(req, res, next){
 });
 
 router.post('/', upload.single('uploadFile'), function(req, res, next){
-    var qid = req.body.qid;
+    var qid = req.file.originalname.split('.')[0];
     var date = req.body.date;
     var era = req.body.era;
     var category = req.body.category.split(',');
     var answer = req.body.answer;
     var score = req.body.score;
-    console.log(req.file.originalname);
+    console.log(req.file.originalname.split('.')[0]);
     db.insertQuestion(qid, date, era, category, answer, score);
     //db upload
     res.redirect('/upload');
